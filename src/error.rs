3de46258890e9e1cbb9c1970c02f62156433c29f
Brandon Richards <bqrichards@icloud.com>
2026-05-28 21:19:00 -0500

    fix: log video download stderr
    Closes #15

--- src/error.rs
+++ src/error.rs
@@ -6,12 +6,16 @@ use tokio::task::JoinError;
 pub enum DownloadError {
     #[error("failed to run video command")]
     Command(#[source] io::Error),
-    #[error("failed to read output of video command")]
-    CommandOutput(#[source] io::Error),
+    #[error("failed to read stdout of video command")]
+    CommandStdoutOutput(#[source] io::Error),
+    #[error("failed to read stderr of video command")]
+    CommandStderrOutput(#[source] io::Error),
     #[error("failed to join on video command")]
     CommandJoin(#[source] JoinError),
     #[error("could not read stdout from video command")]
     CommandNoStdout,
+    #[error("could not read stderr from video command")]
+    CommandNoStderr,
     #[error("video download command exited with no status code")]
     ExitNoCode,
     #[error("video download command exited with status code {0}")]
